26b00fd575a27d6f5a15d7207f9b1ef3186dca24
Андрей Теренков <andreiterenkov@MacBook-Air.local>
2026-01-05 18:30:10 +0000

    Cleanup: Remove 140+ obsolete debug and test files. Repository is now clean and maintainable.

--- fix_rls.sql
+++
@@ -1,5 +0,0 @@
--- Allow public (anon) users to UPDATE statuses (required for toggling checkboxes)
-CREATE POLICY "Allow public update" ON statuses FOR UPDATE USING (true);
-
--- Reload configuration to apply changes immediately
-NOTIFY pgrst, 'reload config';
